088e4226ed633939101a79bd1615baf9a88a5f58
ines-silc <ines.silc@gmail.com>
2018-10-27 16:43:34 +0200

    Prevod

--- zajem.py
+++ zajem.py
@@ -3,48 +3,20 @@ import re
 import os
 import csv
 
-# mapa, v katero bomo shranili podatke
-#data_directory = 'podatki'
-# ime datoteke v katero bomo shranili glavno stran
-#frontpage_filename = 'frontpage.html'
-# ime CSV datoteke v katero bomo shranili podatke
-#csv_filename = 'cat_data.csv'
-
-def read_file_to_string(directory, filename):
-    '''Return the contents of the file "directory"/"filename" as a string.'''
+def preberi_datoteko(directory, filename):
+    '''Funkcija vzame datoteko in jo pretvori v niz'''
     with open(filename, encoding='utf-8') as datoteka:
         return datoteka.read()
 
-
-# Definirajte funkcijo, ki sprejme niz, ki predstavlja vsebino spletne strani,
-# in ga razdeli na dele, kjer vsak del predstavlja en oglas. To storite s
-# pomočjo regularnih izrazov, ki označujejo začetek in konec posameznega
-# oglasa. Funkcija naj vrne seznam nizov.
-
-#Primer vrstice:
-#<td data-label="" class="col-1">
-#<a href="/production/act-s-of-god-lookingglass-theatre-company-2018-2019" title="" data-cms-ai="0">
-#Act(s) of God</a></td>
-#<td data-label="" class="col-2">Lookingglass Theatre Company</td>
-#<td data-label="" class="col-3">Play</td>
-#<td data-label="" class="col-4">Chicago, IL</td>
-#<td data-label="" class="col-5">Regional/ National Tours</td>
-#<td data-label="" class="col-6"></td>
-#</tr><tr class="row-9">
-
-def page_to_ads(directory, datoteka):
-#razdelimo po vrsticah
-    vsebina = read_file_to_string(directory, datoteka)
+def razdelitev(directory, datoteka):
+    '''funkcija razdeli surov niz na podatke o posamezni predstavi'''
+    vsebina = preberi_datoteko(directory, datoteka)
     match = []
     vzorec = r'<td data-label="" class="col-1">' + r'.*?' + r'<td data-label="" class="col-6">'
     for ujemanje in re.finditer(vzorec, vsebina, re.DOTALL):
         ena_vrstica = ujemanje.group(0)
         match.append(ena_vrstica)
     return match
-
-
-# Definirajte funkcijo, ki sprejme niz, ki predstavlja oglas, in izlušči
-# podatke o imenu, ceni in opisu v oglasu.
 
 vzorec = re.compile(
     r'<a href="/production/.*-(?P<time>\d?\d?\d?\d?-\d?\d?\d?\d?)?"'
@@ -65,26 +37,24 @@ def izloci_podatke(ujemanje):
     podatki['type'] = podatki['type'].strip()
     return podatki
 
-podatki_predstav = []
-#to bo seznam slovarjev
-
-def get_dict_from_ad_block(directory, datoteka):
+def pridobi_slovar(directory, datoteka):
     '''Naredi slovar z časom, naslovom, žanrom, lokacijo in tipom predstave'''
-    seznam = page_to_ads(directory, datoteka)
+    seznam = razdelitev(directory, datoteka)
+    podatki_predstav = []
     for predstava in seznam:
-        for ujemanje in vzorec.finditer(predstava):
-            podatki_predstav.append(izloci_podatke(ujemanje))
+        for podatki in vzorec.finditer(predstava):
+            podatki_predstav.append(izloci_podatke(podatki))
     return podatki_predstav
 
 def ads_from_file(directory, filename):
-    '''Parse the ads in filename/directory into a dictionary list.'''
-    predstave = get_dict_from_ad_block(directory, filename)
+    '''Naredi seznam z elementi iz slovarja'''
+    predstave = pridobi_slovar(directory, filename)
     return predstave
 
 ###############################################################################
 
 
-def write_csv(fieldnames, rows, directory, filename):
+def zapisi_csv(fieldnames, rows, directory, filename):
     '''Write a CSV file to directory/filename. The fieldnames must be a list of
     strings, the rows a list of dictionaries each mapping a fieldname to a
     cell-value.'''
@@ -97,17 +67,12 @@ def write_csv(fieldnames, rows, directory, filename):
             writer.writerow(row)
     return None
 
-# Definirajte funkcijo, ki sprejme neprazen seznam slovarjev, ki predstavljajo
-# podatke iz oglasa mačke, in zapiše vse podatke v csv datoteko. Imena za
-# stolpce [fieldnames] pridobite iz slovarjev.
-
-
-def write_data_csv(directory, filename, csv):
+def zapisi_data_csv(directory, filename, csv):
     rows =  ads_from_file(directory, filename)
     fieldnames = ["time","show", "venue", "genre", "location", "type"]
-    write_csv(fieldnames, rows, directory, csv)
+    zapisi_csv(fieldnames, rows, directory, csv)
 
-write_data_csv('podatki', 'podatki/broadway.html', 'broadway.csv')
-#write_data_csv('podatki', 'podatki/offbroadway.html', 'offbroadway.csv')
-#write_data_csv('podatki', 'podatki/london.html', 'london.csv')
-#write_data_csv('podatki', 'podatki/regional.html', 'regional.csv')
+zapisi_data_csv('podatki', 'podatki/broadway.html', 'broadway.csv')
+#zapisi_data_csv('podatki', 'podatki/offbroadway.html', 'offbroadway.csv')
+#zapisi_data_csv('podatki', 'podatki/london.html', 'london.csv')
+#zapisi_data_csv('podatki', 'podatki/regional.html', 'regional.csv')
