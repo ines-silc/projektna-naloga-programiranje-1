acfcab14ff4184166ab2dafb777fb463e393aaf6
ines-silc <ines.silc@gmail.com>
2018-12-19 22:05:19 +0100

    Update obdelava.py

---
+++ obdelava.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+broadway = pd.read_csv('podatki/broadway.csv', encoding = 'cp1252')
+offbroadway = pd.read_csv('podatki/offbroadway.csv', encoding = 'cp1252')
+regional = pd.read_csv('podatki/regional.csv', encoding = 'cp1252')
+london = pd.read_csv('podatki/london.csv', encoding = 'cp1252')
