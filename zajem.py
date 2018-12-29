2a96f948eda7110efa08390e1bfe90bc5632de3a
ines-silc <ines.silc@gmail.com>
2018-12-29 22:20:25 +0100

    Dodanih nekaj virov, popravljen uvoz, začetek opisovanja podatkov

--- zajem.py
+++ zajem.py
@@ -10,7 +10,7 @@ def pripravi_imenik(ime_datoteke):
     if imenik:
         os.makedirs(imenik, exist_ok=True)
 
-def shrani_spletno_stran(url, ime_datoteke, vsili_prenos=False):
+def shrani_spletno_stran(url, ime_datoteke, kodiranje, vsili_prenos=False):
     '''Vsebino strani na danem naslovu shrani v datoteko z danim imenom.'''
     try:
         print('Shranjujem {} ...'.format(url), end='')
@@ -23,12 +23,11 @@ def shrani_spletno_stran(url, ime_datoteke, vsili_prenos=False):
         print('stran ne obstaja!')
     else:
         pripravi_imenik(ime_datoteke)
-        with open(ime_datoteke, 'w', encoding='unicode_internal') as datoteka:
+        with open(ime_datoteke, 'w', encoding=kodiranje) as datoteka:
             datoteka.write(r.text)
             print('shranjeno!')
 
-link = "http://www.playbill.com/productions?q=&venue-type=offbroadway&state=all&genre=all"
-shrani_spletno_stran(link, 'podatki/offbroadway.html')
+
 
 def preberi_datoteko(directory, filename):
     '''Funkcija vzame datoteko in jo pretvori v niz'''
@@ -54,6 +53,8 @@ def preberi_datoteko(directory, filename):
 #    podatki['type'] = podatki['type'].strip()
 #    return podatki
 
+#shrani_spletno_stran("http://www.playbill.com/productions?q=&venue-type=offbroadway&state=all&genre=all", 'podatki/offbroadway.html', 'unicode_internal')
+
 #with open('podatki/broadway.csv', 'w') as datoteka:
 #    vzorec = r'<a href="/production/.*-(?P<time>(?:(?:\d\d\d\d-\d\d\d\d)|(?:\d{10})))?"\s*.*(?:\w|")" data-cms-ai="0">\s*(?P<show>(?:\w|\'|\ |–|:|/|,)*?)\s*</a>\s*</td>\s*<td data-label="" class="col-2">\s*(?P<venue>.*?)\s*</td>\s*<td data-label="" class="col-3">\s*(?P<genre>.*?)\s*</td>\s*<td data-label="" class="col-4">\s*(?P<location>.*?)\s*</td>\s*<td data-label="" class="col-5">\s*(?P<type>.*?)\s*</td>'
 #    writer = csv.writer(datoteka)
@@ -61,12 +62,14 @@ def preberi_datoteko(directory, filename):
 #    for x in re.finditer(vzorec, preberi_datoteko('podatki', 'podatki/broadway.html')):
 #        writer.writerow([x.group(1), x.group(2), x.group(3), x.group(4), x.group(5), x.group(6)])
 
-with open('podatki/offbroadway.csv', 'w') as datoteka:
-    vzorec = '<a  href="/production/.*-(?P<leto>(?:\d\d\d\d-\d\d\d\d)|(?:\d{10}))" title data-cms-ai="0" >\s*(?P<show>(?:\w|\'|\ |–|:|/|,)*)\s*</a>\s*</td>\s*<td data-label="" class="col-2">\s*(?P<venue>.*)\s*</td>\s*<td data-label="" class="col-3">\s*(?P<genre>.*)\s*</td>\s*<td data-label="" class="col-4">\s*(?P<location>.*)\s*</td>\s*<td data-label="" class="col-5">\s*(?P<type>.*)\s*</td>'
-    writer = csv.writer(datoteka)
-    writer.writerow(['time', 'show', 'venue', 'genre', 'location', 'type'])
-    for x in re.finditer(vzorec, preberi_datoteko('podatki', 'podatki/offbroadway.html')):
-        writer.writerow([x.group(1), x.group(2), x.group(3), x.group(4), x.group(5), x.group(6)])
+
+#with open('podatki/offbroadway.csv', 'w') as datoteka:
+#    vzorec = '<a  href="/production/.*-(?P<leto>(?:\d\d\d\d-\d\d\d\d)|(?:\d{10}))" title data-cms-ai="0" >\s*(?P<show>(?:\w|\'|\ |–|:|/|,)*)\s*</a>\s*</td>\s*<td data-label="" class="col-2">\s*(?P<venue>.*)\s*</td>\s*<td data-label="" class="col-3">\s*(?P<genre>.*)\s*</td>\s*<td data-label="" class="col-4">\s*(?P<location>.*)\s*</td>\s*<td data-label="" class="col-5">\s*(?P<type>.*)\s*</td>'
+#    writer = csv.writer(datoteka)
+#    writer.writerow(['time', 'show', 'venue', 'genre', 'location', 'type'])
+#    for x in re.finditer(vzorec, preberi_datoteko('podatki', 'podatki/offbroadway.html')):
+#        writer.writerow([x.group(1), x.group(2), x.group(3), x.group(4), x.group(5), x.group(6)])
+
 
 #with open('podatki/regional.csv', 'w') as datoteka:
 #    vzorec = r'<a href="/production/.*-(?P<time>(?:(?:\d\d\d\d-\d\d\d\d)|(?:\d{10})))?" title=""(?: data-cms-ai="0">|><span class="data-value">)\s*(?P<show>(?:\w|\'|\ |–|:|/|,)*?)\s*(?:</a>|</span></a>)\s*</td>\s*<td (?:ss="")? data-label=".*" class="col-2">(?:\s*<span class="data-value">)?\s*(?P<venue>.*?)\s*(?:</span>?\s*</td>|</td>)\s*<td(?: ss="")? data-label=".*" class="col-3">(?:\s*<span class="data-value">)?\s*(?P<genre>.*?)(?:\s*</span>)?\s*</td>\s*<td(?: ss="")? data-label="\w*" class="col-4">\s*(?P<location>.*?)\s*</td>\s*<td(?: ss="")? data-label=".*" class="col-5">\s*(?:\s*<span class="data-value">)*(?P<type>.*?)\s*(?:</span>)?\s*</td>'
@@ -82,3 +85,54 @@ with open('podatki/offbroadway.csv', 'w') as datoteka:
 #    writer.writerow(['time', 'show', 'venue', 'genre', 'location', 'type'])
 #    for x in re.finditer(vzorec, preberi_datoteko('podatki', 'podatki/london.html')):
 #        writer.writerow([x.group(1), x.group(2), x.group(3), x.group(4), x.group(5), x.group(6)])
+
+
+#shrani_spletno_stran('https://www.broadwayleague.com/research/statistics-broadway-nyc/', 'podatki/broadway_statistika.html', 'utf-8')
+
+with open('podatki/broadway_statistika.csv', 'w', encoding = 'utf-8') as datoteka:
+    vzorec = r'<td>(?P<Season>(?:\d\d\d\d-\d\d)).*</td>\s*<td>\$(?P<Gross>(?:\d,\d\d\d)|(\d\d\d))</td>\s*<td>(?P<Attendance>\d*\.?\d*)</td>\s*<td>(?P<Playing_Weeks>(?:\d,\d\d\d)|(\d\d\d))</td>\s*<td>(?P<New_Productions>(?:\d+))</td>'
+    writer = csv.writer(datoteka)
+    writer.writerow(['season', 'gross', 'attendance', 'duration', 'newProductions'])
+    for x in re.finditer(vzorec, preberi_datoteko('podatki', 'podatki/broadway_statistika.html')):
+        writer.writerow([x.group(1), x.group(2), x.group(3), x.group(4), x.group(5)])
+
+
+#shrani_spletno_stran('https://www.broadwayleague.com/research/statistics-touring-broadway/', 'podatki/regional_statistika.html', 'utf-8')
+
+with open('podatki/regional_statistika.csv', 'w', encoding = 'utf-8') as datoteka:
+    vzorec = r'<td>(?P<Season>(?:\d\d\d\d-\d\d)).*</td>\s*<td>\$?(?P<Gross>(?:\d,\d\d\d)|(\d\d\d))</td>\s*<td>(?P<Attendance>(?:\d*\.?\d*))</td>\s*<td>(?P<Playing_Weeks>(?:\d,\d\d\d)|(\d\d\d))</td>'
+    writer = csv.writer(datoteka)
+    writer.writerow(['season', 'gross', 'attendance', 'duration'])
+    for x in re.finditer(vzorec, preberi_datoteko('podatki', 'podatki/regional_statistika.html')):
+        writer.writerow([x.group(1), x.group(2), x.group(3), x.group(4)])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
